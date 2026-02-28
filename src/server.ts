dcbfdccc0e48c35bac7e3d66c84ab14bad0b91e1
Luara Kerlen dos Santos Rangel <luarakerlen@hotmail.com>
2026-02-28 19:46:32 -0300

    Makes server port configurable via environment
    Improves deployment flexibility by allowing the server port to be set from an environment variable.
    Expands TypeScript config comments for easier customization and future maintenance.

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,15 @@ import studentsRoutes from './routes/students.routes';
 import cors from 'cors';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.json()); // body parser
 app.use(cors()); // habilita todos os verbos para todas as origens
 
 app.use(studentsRoutes);
 
-app.listen(port, () => {
-	console.log(`Server is running on port ${port}`);
+app.listen(PORT, () => {
+	console.log(`Server is running on port ${PORT}`);
 });
 
 // growdev.com.br (cliente/servidor) > HTTP (CORS) > growlabs.com.br (servidor)
